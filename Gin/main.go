9e2c03eb99de62a3d206de2443216ff14455d38c
Lee Joonki <nuts_love86@yahoo.co.jp>
2024-01-09 09:03:52 +0900

    modified

--- Gin/main.go
+++ Gin/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
 
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
+	csrf "github.com/utrack/gin-csrf"
 
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
@@ -74,17 +78,37 @@ func main() {
 	otel.SetTextMapPropagator(propagation.TraceContext{})
 
 	router = gin.Default()
-	// 静的ファイルのルーティング
-	router.Static("/static", "./static")
+	router.Static("/static", "./static") // 静的ファイルの読み込み
 	router.Use(TracingMiddleware())
-	router.LoadHTMLGlob("templates/*")
-	//router.LoadHTMLFiles("templates/template1.html", "templates/template2.html")
+	router.LoadHTMLGlob("templates/*") // テンプレートファイルの読み込み
+
+	// Cookieベースのセッションを設定
+	secretKey := os.Getenv("SESSION_SECRET_KEY") // Sessionの暗号化キーは固定の値を使用することで、アプリの再起動時にセッションが維持されるようにする
+	if secretKey == "" {
+		fmt.Println("SESSION_SECRET_KEY環境変数が設定されていません")
+		return
+	}
+
+	store := cookie.NewStore([]byte(secretKey))
+	router.Use(sessions.Sessions("session", store)) // ブラウザのCookieにセッションIDを保存する
+
+	// CSRFミドルウェアの設定
+	// HTML内の_csrfの値を取得して、リクエストトークンと比較を行い、一致しない場合ErrorFuncを実行する（https://github.com/utrack/gin-csrf/blob/master/csrf.go）
+	router.Use(csrf.Middleware(csrf.Options{
+		Secret: secretKey, // 上のCookieベースのセッションと同じ値を指定
+		ErrorFunc: func(c *gin.Context) {
+			c.String(400, "CSRF token mismatch")
+			c.Abort()
+		},
+	}))
 
 	router.GET("/", func(c *gin.Context) {
+		token := csrf.GetToken(c)
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{
 			"head_title": "Golang-Gin",
 			"title":      "Main website",
 			"content":    "OpenTelemetry with Golang-Gin",
+			"csrfToken":  token,
 		})
 	})
 
