18268d68d2e915d2a418482b79ea55a6bf636639
Lee Joonki <nuts_love86@yahoo.co.jp>
2024-06-17 19:02:32 +0900

    test

--- Gin/ham3/config/opensearch.go
+++ Gin/ham3/config/opensearch.go
@@ -30,94 +30,96 @@ flavor:
 			memory: 1Gi
 		jvm_heap: 512M
 		jvm_perm: 128M
-	m1.small:
-		requests:
-			cpu: 250m
-			memory: 1280Mi
-		limits:
-			cpu: 1000m
-			memory: 2Gi
-		jvm_heap: 1G
-		jvm_perm: 256M
-	m1.medium:
-		requests:
-			cpu: 500m
-			memory: 4352Mi
-		limits:
-			cpu: 2000m
-			memory: 8Gi
-		jvm_heap: 4G
-		jvm_perm: 256M
-	m1.large:
-		requests:
-			cpu: 1000m
-			memory: 8448Mi
-		limits:
-			cpu: 4000m
-			memory: 16Gi
-		jvm_heap: 8G
-		jvm_perm: 256M
-	m1.xlarge:
-		requests:
-			cpu: 2000m
-			memory: 16640Mi
-		limits:
-			cpu: 8000m
-			memory: 32Gi
-		jvm_heap: 16G
-		jvm_perm: 256M
-	d1.tiny:
-		requests:
-			cpu: 125m
-			memory: 1Gi
-		limits:
-			cpu: 500m
-			memory: 1Gi
-		jvm_heap: 512M
-		jvm_perm: 128M
-	d1.small:
-		requests:
-			cpu: 250m
-			memory: 2Gi
-		limits:
-			cpu: 1000m
-			memory: 2Gi
-		jvm_heap: 1G
-		jvm_perm: 256M
-	d1.medium:
-		requests:
-			cpu: 500m
-			memory: 8Gi
-		limits:
-			cpu: 2000m
-			memory: 8Gi
-		jvm_heap: 4G
-		jvm_perm: 256M
-	d1.large:
-		requests:
-			cpu: 1000m
-			memory: 16Gi
-		limits:
-			cpu: 4000m
-			memory: 16Gi
-		jvm_heap: 8G
-		jvm_perm: 256M
-	d1.mlarge:
-		requests:
-			cpu: 1000m
-			memory: 32Gi
-		limits:
-			cpu: 4000m
-			memory: 32Gi
-		jvm_heap: 16G
-		jvm_perm: 256M
-	d1.xlarge:
-		requests:
-			cpu: 2000m
-			memory: 32Gi
-		limits:
-			cpu: 8000m
-			memory: 32Gi
-		jvm_heap: 16G
-		jvm_perm: 256M
 `
+
+// 		m1.small:
+// 		requests:
+// 			cpu: 250m
+// 			memory: 1280Mi
+// 		limits:
+// 			cpu: 1000m
+// 			memory: 2Gi
+// 		jvm_heap: 1G
+// 		jvm_perm: 256M
+// 	m1.medium:
+// 		requests:
+// 			cpu: 500m
+// 			memory: 4352Mi
+// 		limits:
+// 			cpu: 2000m
+// 			memory: 8Gi
+// 		jvm_heap: 4G
+// 		jvm_perm: 256M
+// 	m1.large:
+// 		requests:
+// 			cpu: 1000m
+// 			memory: 8448Mi
+// 		limits:
+// 			cpu: 4000m
+// 			memory: 16Gi
+// 		jvm_heap: 8G
+// 		jvm_perm: 256M
+// 	m1.xlarge:
+// 		requests:
+// 			cpu: 2000m
+// 			memory: 16640Mi
+// 		limits:
+// 			cpu: 8000m
+// 			memory: 32Gi
+// 		jvm_heap: 16G
+// 		jvm_perm: 256M
+// 	d1.tiny:
+// 		requests:
+// 			cpu: 125m
+// 			memory: 1Gi
+// 		limits:
+// 			cpu: 500m
+// 			memory: 1Gi
+// 		jvm_heap: 512M
+// 		jvm_perm: 128M
+// 	d1.small:
+// 		requests:
+// 			cpu: 250m
+// 			memory: 2Gi
+// 		limits:
+// 			cpu: 1000m
+// 			memory: 2Gi
+// 		jvm_heap: 1G
+// 		jvm_perm: 256M
+// 	d1.medium:
+// 		requests:
+// 			cpu: 500m
+// 			memory: 8Gi
+// 		limits:
+// 			cpu: 2000m
+// 			memory: 8Gi
+// 		jvm_heap: 4G
+// 		jvm_perm: 256M
+// 	d1.large:
+// 		requests:
+// 			cpu: 1000m
+// 			memory: 16Gi
+// 		limits:
+// 			cpu: 4000m
+// 			memory: 16Gi
+// 		jvm_heap: 8G
+// 		jvm_perm: 256M
+// 	d1.mlarge:
+// 		requests:
+// 			cpu: 1000m
+// 			memory: 32Gi
+// 		limits:
+// 			cpu: 4000m
+// 			memory: 32Gi
+// 		jvm_heap: 16G
+// 		jvm_perm: 256M
+// 	d1.xlarge:
+// 		requests:
+// 			cpu: 2000m
+// 			memory: 32Gi
+// 		limits:
+// 			cpu: 8000m
+// 			memory: 32Gi
+// 		jvm_heap: 16G
+// 		jvm_perm: 256M
+// `
