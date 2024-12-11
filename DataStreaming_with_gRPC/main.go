bceec0eff6eabbe48002772854d08bb166f8cff3
Cloud User <centos@ip-10-111-1-195.ap-northeast-1.compute.internal>
2024-12-11 17:43:27 +0000

    modified

--- DataStreaming_with_gRPC/main.go
+++ DataStreaming_with_gRPC/main.go
@@ -1,22 +1,18 @@
 package main
 
 import (
-	"fmt"
 	_ "io/ioutil"
-	"log"
-	_ "log"
-	"protobuf/proto/pb"
 
-	"github.com/golang/protobuf/jsonpb"
+	_ "github.com/golang/protobuf/jsonpb"
 	_ "google.golang.org/protobuf/proto"
 )
 
 func main() {
-	employee := &pb.Employee{
-		Id:    1,
-		Name:  "Lee",
-		Email: "test@example.com",
-	}
+	// employee := &pb.Employee{
+	// 	Id:    1,
+	// 	Name:  "Lee",
+	// 	Email: "test@example.com",
+	// }
 
 	// binData, err := proto.Marshal(employee)
 	// if err != nil {
@@ -41,18 +37,18 @@ func main() {
 
 	// fmt.Println(readEmployee)
 
-	m := jsonpb.Marshaler{}
-	out, err := m.MarshalToString(employee)
-	if err != nil {
-		log.Fatalln("Can't marshal to json", err)
-	}
+	// m := jsonpb.Marshaler{}
+	// out, err := m.MarshalToString(employee)
+	// if err != nil {
+	// 	log.Fatalln("Can't marshal to json", err)
+	// }
 
 	// fmt.Println(out)
 
-	readEmployee := &pb.Employee{}
-	if err := jsonpb.UnmarshalString(out, readEmployee); err != nil {
-		log.Fatalln("Can't unmarshal from json", err)
-	}
+	// readEmployee := &pb.Employee{}
+	// if err := jsonpb.UnmarshalString(out, readEmployee); err != nil {
+	// 	log.Fatalln("Can't unmarshal from json", err)
+	// }
 
-	fmt.Println(readEmployee)
+	// fmt.Println(readEmployee)
 }
