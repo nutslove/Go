d71ffd1db2f62ef139220ed379d99575803da9ed
Lee Joonki <nuts_love86@yahoo.co.jp>
2025-03-15 00:52:26 +0900

    modified

--- LogToOTLPhttp/main.go
+++ LogToOTLPhttp/main.go
@@ -2,15 +2,26 @@ package main
 
 import (
 	"bufio"
+	"compress/gzip"
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
 	"log"
 	"log/slog"
 	"os"
+	"regexp"
+	"strings"
 	"time"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
+	"github.com/aws/aws-sdk-go-v2/service/sqs"
+
 	"go.opentelemetry.io/contrib/bridges/otelslog"
-	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploghttp"
 	"go.opentelemetry.io/otel/log/global"
 	sdk "go.opentelemetry.io/otel/sdk/log"
@@ -18,20 +29,48 @@ import (
 	semconv "go.opentelemetry.io/otel/semconv/v1.24.0"
 )
 
+var (
+	QueueURL     string = getEnvironmentVariable("SQS_QUEUE_URL", "https://sqs.ap-northeast-1.amazonaws.com/637423497892/lee-test-standard-queue-for-alb-cloudfront-logs")
+	otlpEndpoint string = getEnvironmentVariable("OTLP_ENDPOINT", "10.1.2.218:31100")
+)
+
+func getEnvironmentVariable(key string, defaultValue string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return defaultValue
+	}
+	return value
+}
+
+type S3EventNotification struct {
+	Records []struct {
+		S3 struct {
+			Bucket struct {
+				Name string `json:"name"`
+				Arn  string `json:"arn"`
+			} `json:"bucket"`
+			Object struct {
+				Key  string `json:"key"`
+				Size int    `json:"size"`
+				ETag string `json:"eTag"`
+			} `json:"object"`
+		} `json:"s3"`
+	} `json:"Records"`
+}
+
 // OTLPClient はOpenTelemetryを使ってログを送信するクライアント
 type OTLPClient struct {
 	logger         *slog.Logger
 	loggerProvider *sdk.LoggerProvider
+	sid            string
 	attributes     map[string]string
 }
 
-// NewOTLPClient は新しいOTLPクライアントを作成します
-func NewOTLPClient(ctx context.Context, sid string, endpoint string, attributes map[string]string) (*OTLPClient, error) {
+func NewOTLPClient(ctx context.Context, sid string, attributes map[string]string) (*OTLPClient, error) {
 	// リソース情報の設定
-	serviceName := "logger"
 	res, err := resource.New(ctx,
 		resource.WithAttributes(
-			semconv.ServiceName(serviceName),
+			semconv.ServiceName(sid),
 		),
 	)
 	if err != nil {
@@ -45,7 +84,7 @@ func NewOTLPClient(ctx context.Context, sid string, endpoint string, attributes 
 
 	// OTLP HTTP エクスポーターの作成
 	exporter, err := otlploghttp.New(ctx,
-		otlploghttp.WithEndpoint(endpoint),
+		otlploghttp.WithEndpoint(otlpEndpoint),
 		otlploghttp.WithURLPath("/otlp/v1/logs"),
 		otlploghttp.WithHeaders(headers),
 		otlploghttp.WithInsecure(), // 本番環境ではTLSを使用すること
@@ -54,9 +93,6 @@ func NewOTLPClient(ctx context.Context, sid string, endpoint string, attributes 
 		return nil, fmt.Errorf("failed to create log exporter: %w", err)
 	}
 
-	// バッチ処理の設定を最適化
-	// 注意: 最新のOpenTelemetry SDKでは設定オプションが異なる可能性があります
-	// 実際のSDKのバージョンに合わせて適切なオプションを使用してください
 	loggerProvider := sdk.NewLoggerProvider(
 		sdk.WithResource(res),
 		sdk.WithProcessor(sdk.NewBatchProcessor(exporter)),
@@ -66,45 +102,153 @@ func NewOTLPClient(ctx context.Context, sid string, endpoint string, attributes 
 	global.SetLoggerProvider(loggerProvider)
 
 	// ロガー作成
-	logger := otelslog.NewLogger(serviceName)
+	logger := otelslog.NewLogger("otlphttp-logger") // "scope_name"というStructured metadataとして設定される
 
 	return &OTLPClient{
 		logger:         logger,
 		loggerProvider: loggerProvider,
+		sid:            sid,
 		attributes:     attributes,
 	}, nil
 }
 
 // SendLog はログをOpenTelemetryに送信します
-func (c *OTLPClient) SendLog(message string) {
-	// 空のログは無視
-	if message == "" {
+func (c *OTLPClient) SendLog(cfg aws.Config, bucketName string, object string) {
+	// ログを確実に送信し、シャットダウン
+	defer func() {
+		if err := c.FlushLogs(10 * time.Second); err != nil {
+			log.Printf("Failed to flush logs: %v", err)
+		}
+		if err := c.Shutdown(10 * time.Second); err != nil {
+			log.Printf("Failed to shutdown client: %v", err)
+		}
+	}()
+
+	fmt.Printf("SendLog Goroutine started (%s)\n", object)
+	startTime := time.Now()
+
+	s3_client := s3.NewFromConfig(cfg)
+	result, err := s3_client.GetObject(context.TODO(), &s3.GetObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(object),
+	})
+	if err != nil {
+		var noKey *types.NoSuchKey
+		if errors.As(err, &noKey) {
+			log.Printf("Can't get object %s from bucket %s. No such key exists.\n", object, bucketName)
+			err = noKey
+		} else {
+			log.Printf("Couldn't get object %v:%v. Here's why: %v\n", bucketName, object, err)
+		}
+		return
+	}
+	defer result.Body.Close()
+	splitFileName := strings.Split(object, "/")
+	gzipFileName := splitFileName[len(splitFileName)-1]
+
+	file, err := os.Create(gzipFileName)
+	if err != nil {
+		log.Printf("Couldn't create file %v. Here's why: %v\n", gzipFileName, err)
 		return
 	}
 
-	// 属性の設定
-	attrs := []attribute.KeyValue{}
-	for k, v := range c.attributes {
-		attrs = append(attrs, attribute.String(k, v))
+	// S3から取得したデータをファイルに書き込む
+	_, err = io.Copy(file, result.Body)
+	if err != nil {
+		log.Printf("Couldn't copy S3 object to file. Here's why: %v\n", err)
+		file.Close()
+		return
+	}
+	file.Close()
+
+	gzipFile, err := os.Open(gzipFileName)
+	if err != nil {
+		log.Printf("Could not open file %s: %v\n", gzipFileName, err)
+		return
+	}
+	defer gzipFile.Close()
+
+	gzipReader, err := gzip.NewReader(gzipFile)
+	if err != nil {
+		log.Printf("Could not create gzip reader: %v\n", err)
+		return
+	}
+	defer gzipReader.Close()
+
+	// 出力先ファイルを作成
+	outputFile, err := os.Create(strings.TrimSuffix(gzipFileName, ".gz"))
+	if err != nil {
+		log.Printf("Could not create output file: %v\n", err)
+		return
+	}
+	defer outputFile.Close()
+
+	// 解凍したデータを出力ファイルにコピー
+	_, err = io.Copy(outputFile, gzipReader)
+	if err != nil {
+		log.Printf("Could not copy data to output file: %v\n", err)
+		return
 	}
 
-	// ログレベルに応じた送信 - ここではすべてエラーレベルとして送信
-	// TODO: ALB,CloudFrontのログの場合、ログの中からステータスコードを取得して、それにあったログレベルを設定する
-	// c.logger.Error(message)
-	// c.logger.Info(message)
-	c.logger.Warn(message)
-	// c.logger.Debug(message)
-	// c.logger.Log(message)
+	// ファイルポインタを先頭に戻す
+	_, err = outputFile.Seek(0, 0)
+	if err != nil {
+		log.Printf("Could not reset file pointer: %v\n", err)
+		return
+	}
+
+	// 処理完了後、一時ファイルを削除
+	defer func() {
+		if err := os.Remove(gzipFileName); err != nil {
+			log.Printf("Could not delete gzip file: %v\n", gzipFileName)
+		}
+		if err := os.Remove(strings.TrimSuffix(gzipFileName, ".gz")); err != nil {
+			log.Printf("Could not delete decompressed file: %v\n", strings.TrimSuffix(gzipFileName, ".gz"))
+		}
+	}()
+
+	scanner := bufio.NewScanner(outputFile) // ファイルを行ごとに読み込む
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		// 空のログは無視
+		if line == "" {
+			continue
+		}
+
+		var status_code string
+		if c.attributes["logtype"] == "alb" {
+			status_code = strings.Split(line, " ")[8]
+		} else if c.attributes["logtype"] == "cloudfront" || c.attributes["logtype"] == "cdn" {
+			if strings.Contains(line, "#Version:") || strings.Contains(line, "#Fields:") {
+				continue // ヘッダー行は無視
+			}
+			status_code = strings.Split(line, "\t")[8]
+		}
+
+		// ログレベルに応じた送信
+		switch {
+		case regexp.MustCompile(`5\d{2}`).MatchString(status_code):
+			c.logger.Error(line, "statuscode", status_code, "logtype", c.attributes["logtype"], "environment", c.attributes["environment"])
+		case regexp.MustCompile(`4\d{2}`).MatchString(status_code):
+			c.logger.Warn(line, "statuscode", status_code, "logtype", c.attributes["logtype"], "environment", c.attributes["environment"])
+		case regexp.MustCompile(`[2-3]\d{2}`).MatchString(status_code):
+			c.logger.Info(line, "statuscode", status_code, "logtype", c.attributes["logtype"], "environment", c.attributes["environment"])
+		default:
+			c.logger.Info(line, "statuscode", status_code, "logtype", c.attributes["logtype"], "environment", c.attributes["environment"])
+		}
+	}
+	fmt.Printf("SendLog Goroutine finished (%s). runtime: %s\n", object, time.Since(startTime))
 }
 
-// FlushLogs はログのバッチを強制的に送信します
+// ログのバッチを強制的に送信
 func (c *OTLPClient) FlushLogs(timeout time.Duration) error {
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
 	return c.loggerProvider.ForceFlush(ctx)
 }
 
-// Shutdown はOTLPクライアントを正常にシャットダウンします
+// OTLPクライアントを正常にシャットダウン
 func (c *OTLPClient) Shutdown(timeout time.Duration) error {
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
@@ -112,87 +256,75 @@ func (c *OTLPClient) Shutdown(timeout time.Duration) error {
 }
 
 func main() {
-	ctx := context.Background()
-
-	// OpenTelemetryクライアントの設定
-	endpoint := "192.168.0.177:31100"
-
-	// カスタム属性の設定
-	attributes := map[string]string{
-		"environment": "development",
-		"application": "log-forwarder",
-		"LogType":     "alb",
-	}
-
-	// OTLPクライアントの作成
-	otlpClient, err := NewOTLPClient(ctx, "fake", endpoint, attributes)
+	cfg, err := config.LoadDefaultConfig(
+		context.TODO(),
+		config.WithRegion("ap-northeast-1"),
+	)
 	if err != nil {
-		log.Fatalf("Failed to create OTLP client: %v", err)
+		fmt.Println("Error creating config:", err)
+		return
 	}
 
-	// アプリケーション終了時にログを確実に送信し、シャットダウン
-	defer func() {
-		if err := otlpClient.FlushLogs(5 * time.Second); err != nil {
-			log.Printf("Failed to flush logs: %v", err)
-		}
-		if err := otlpClient.Shutdown(5 * time.Second); err != nil {
-			log.Printf("Failed to shutdown client: %v", err)
-		}
-	}()
+	sqs_client := sqs.NewFromConfig(cfg)
 
-	filePath := "/mnt/c/Users/nuts_/Github/Go/LogToOTLPhttp/log.txt"
+	// キューからメッセージを受信するためのパラメータ
+	receiveParams := &sqs.ReceiveMessageInput{
+		QueueUrl:            aws.String(QueueURL),
+		MaxNumberOfMessages: 10, // 一度に受信する最大メッセージ数
+		VisibilityTimeout:   30, // メッセージが他の受信者から見えなくなる時間（秒）
+		WaitTimeSeconds:     20, // ロングポーリングを有効にする時間（秒）
+		// WaitTimeSeconds:     0, // ロングポーリングを無効にする
+	}
 
 	for {
-		// ファイルの存在確認
-		_, err := os.Stat(filePath)
-		if os.IsNotExist(err) {
-			log.Printf("ファイルは存在しません: %s\n", filePath)
-			time.Sleep(300 * time.Second)
-			continue
-		} else if err != nil {
-			log.Printf("エラー: %v\n", err)
-			time.Sleep(300 * time.Second)
-			continue
-		}
-
-		log.Printf("ファイルが存在します: %s - 内容を読み込みます\n", filePath)
-		// ファイルを開く
-		file, err := os.Open(filePath)
+		resp, err := sqs_client.ReceiveMessage(context.TODO(), receiveParams)
 		if err != nil {
-			log.Printf("ファイルを開けませんでした: %v\n", err)
-			time.Sleep(300 * time.Second)
+			fmt.Println("Error receiving message from SQS:", err)
 			continue
 		}
 
-		// ファイルを行ごとに読み込んで直接OpenTelemetryに送信
-		scanner := bufio.NewScanner(file)
-		lineCount := 0
-
-		log.Println("ファイルの内容:")
-		for scanner.Scan() {
-			lineCount++
-			line := scanner.Text()
-
-			// ログを直接OpenTelemetryに送信
-			otlpClient.SendLog(line)
-
-			log.Printf("行 %d: %s\n", lineCount, line)
+		if len(resp.Messages) == 0 {
+			fmt.Println("Received no messages")
+			continue
 		}
 
-		if err := scanner.Err(); err != nil {
-			log.Printf("ファイル読み込み中にエラーが発生しました: %v\n", err)
+		for _, message := range resp.Messages {
+			var s3Event S3EventNotification
+			if err := json.Unmarshal([]byte(*message.Body), &s3Event); err != nil {
+				fmt.Println("Error unmarshalling message:", err)
+				continue
+			}
+
+			for _, record := range s3Event.Records {
+				logType := strings.Split(record.S3.Object.Key, "/")[0]
+				sid := strings.Split((strings.Split(record.S3.Object.Key, "/")[1]), "-")[1]
+				environment := strings.Split((strings.Split(record.S3.Object.Key, "/")[1]), "-")[0]
+
+				attributes := map[string]string{
+					"environment": environment,
+					"logtype":     logType,
+				}
+
+				// OTLPクライアントの作成
+				otlpClient, err := NewOTLPClient(context.Background(), sid, attributes)
+				if err != nil {
+					log.Fatalf("Failed to create OTLP client: %v", err)
+				}
+
+				go otlpClient.SendLog(cfg, record.S3.Bucket.Name, record.S3.Object.Key)
+			}
+
+			// 処理済みとしてメッセージを削除
+			_, err := sqs_client.DeleteMessage(context.TODO(), &sqs.DeleteMessageInput{
+				QueueUrl:      aws.String(QueueURL),
+				ReceiptHandle: message.ReceiptHandle,
+			})
+			if err != nil {
+				fmt.Printf("Error deleting message: %v\n", err)
+			}
 		}
-
-		// 読み込み完了後、バッファに残っているログを確実に送信
-		if err := otlpClient.FlushLogs(5 * time.Second); err != nil {
-			log.Printf("Failed to flush logs after reading file: %v", err)
-		}
-
-		log.Printf("ファイル読み込み完了。合計 %d 行\n", lineCount)
-
-		file.Close()
 
 		// 次の確認まで待機
-		time.Sleep(300 * time.Second)
+		time.Sleep(1 * time.Second)
 	}
 }
